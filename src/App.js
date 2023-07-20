c52e77967a01c54c25dcad1673b5ac56d1702139
fahim-muntasir <fahimmuntasir0909@gmail.com>
2023-07-20 11:37:06 +0600

    create products and single products page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Card from "./components/pages/Card";
 import Home from "./components/pages/Home";
+import Product from "./components/pages/Product";
+import Products from "./components/pages/Products";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/card" element={<Card />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/card" element={<Card />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/products/:id" element={<Product />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
